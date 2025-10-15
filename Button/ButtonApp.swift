5bfc348c18be477e791b52fc9d1c57bb0087fe6d
Kate Buyanova <buyanovarina@gmail.com>
2025-10-15 16:37:21 +0200

    Initial Commit

---
+++ Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by Katerina Puntsel on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonTests/ButtonTests.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTests.swift
+//  ButtonTests
+//
+//  Created by Katerina Puntsel on 15/10/2025.
+//
+
+import Testing
+@testable import Button
+
+struct ButtonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
